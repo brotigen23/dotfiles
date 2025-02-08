b0af9b57dce15a544ce9ea46455fbd39dc66fc77
brotigen23 <brotigen23@gmail.com>
2025-02-08 15:01:23 +0300

    fix: update.sh

--- update.sh
+++ update.sh
@@ -1,19 +1,58 @@
 #!/bin/bash
-#Bash
+
+function updateBash {
 rm -rf .config/bash
 cp -r ~/.config/bash .config
 rm .bashrc
 cp ~/.bashrc .
+echo 'bash config updated'
+}
 
-# Nvim
+function updateNvim {
 rm -rf .config/nvim
-cp -r ~/.config/nvim .config
+cp -r ~/.config/nvim .config/
+echo 'nvim config updated'
+}
 
-
-# Helix
+function updateHelix {
 rm -rf .config/helix
-cp -r ~/.config/helix
+cp -r ~/.config/helix .config/
+echo 'helix config updated'
+}
 
-# Polybar
+function updatePolybar {
 rm -rf .config/polybar
-cp -r ~/.config/polybar
+cp -r ~/.config/polybar .config/
+echo 'polybar config updated'
+}
+
+echo '
+Choose dir:
+1 - all
+2 - bash
+3 - helix
+4 - polybar
+фыв 
+'
+
+read key
+
+case "$key" in
+  "1"  )
+
+  ;;
+  "2"  )
+  ;;
+  "3"  )
+  updateHelix
+  ;;
+  "4"  )
+  updatePolybar
+  ;;
+  *    )
+
+  ;;
+esac
+
+exit 0
+
