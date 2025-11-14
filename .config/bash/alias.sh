e721ff411eba9bca0aecec1addbde3544bbe3658
brotigen23 <brotigen23@gmail.com>
2025-11-14 18:26:49 +0300

    bash: add server, change lazygit path

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -3,4 +3,6 @@ alias la='ls -A'
 alias l='ls -CF'
 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-alias ldotfiles='/usr/local/bin/lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+alias ldotfiles='/usr/bin/lazygit --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+
+alias server='python -m http.server 8080'
