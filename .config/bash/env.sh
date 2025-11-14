7cce278a5f6db2a14700b0f15ebb9040bfedc54d
brotigen23 <brotigen23@gmail.com>
2025-11-14 16:58:12 +0300

    bash: add user specific env

--- .config/bash/env.sh
+++ .config/bash/env.sh
@@ -1,19 +1,12 @@
-GOPATH="$(asdf where golang)"
-
-export PATH="$GOPATH/bin:$PATH"
-
-# Настройка pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d "$PYENV_ROOT/bin" ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init - bash)"
-
-# Настройка Rust (cargo)
-[ -f "$HOME/.cargo/env" ] && . "$HOME/.cargo/env"
+# User specific environment
+if ! [[ "$PATH" =~ "$HOME/.local/bin:$HOME/bin:" ]]; then
+    PATH="$HOME/.local/bin:$HOME/bin:$PATH"
+fi
+export PATH
 
 # Настройка ASDF
-export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
+export PATH="$HOME/.asdf/shims:$PATH"
 
-# Настройка NVM (если используешь Node.js)
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-
+# GOPATH
+GOPATH="$(asdf where golang)"
+export PATH="$GOPATH/bin:$PATH"
